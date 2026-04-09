e1084f9ed9f8935e54904945a8600d7362f7d4ce
Vaughn Vernon <vaughn@kalele.io>
2026-04-08 19:01:52 -0700

    Changed license from RPL 1.5 to LGPL 3 and updated copyright notices.

--- examples/EncapsulationDemo.ts
+++ examples/EncapsulationDemo.ts
@@ -1,3 +1,23 @@
+// Copyright © 2012-2026 Vaughn Vernon. All rights reserved.
+// Copyright © 2012-2026 Kalele, Inc. All rights reserved.
+//
+// See: LICENSE.md in repository root directory
+//
+// This file is part of DomoActors-TS.
+//
+// DomoActors-TS is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// DomoActors-TS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with DomoActors-TS. If not, see <https://www.gnu.org/licenses/>.
+
 /**
  * Demonstration of environment() encapsulation
  *
